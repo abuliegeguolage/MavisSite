32406708b5d5b0a08d0a6bb3d03474aa387b3546
abuliegeguolage <123>
2024-12-22 14:53:47 +0800

    feat: header & overall style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <RouterView />
+  <Header></Header>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+main {
+  margin: auto;
+  width: 70vw;
+  min-height: 90vh;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import navis from '../assets/navis.json';
+
+let drawer = ref(false);
+
+</script>
+
+<template>
+    <section class="header">
+        <RouterLink to="/">
+            <v-btn variant="text">
+                <h2 class="logo">
+                    <span class="shadow">
+                        Site
+                    </span>
+                    <span class="noumenon">
+                        Mavis
+                    </span>
+
+                </h2>
+            </v-btn>
+        </RouterLink>
+        <section class="navis">
+            <ul class="tabs">
+                <li v-for="item in navis">
+                    <RouterLink :to="item.path">
+                        <v-btn variant="text" class="navi-btn">
+                            {{ item.name }}
+                        </v-btn>
+                    </RouterLink>
+                </li>
+            </ul>
+            <v-layout>
+                <v-navigation-drawer v-model="drawer" temporary class="drawer">
+                    <v-list density="compact" nav>
+                        <RouterLink v-for="item in navis" :to="item.path">
+                            <v-list-item>
+                                {{ item.name }}
+                            </v-list-item>
+                        </RouterLink>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-layout>
+
+            <v-btn @click="drawer = !drawer" variant="text" class="tab-folder navi-tab">...</v-btn>
+        </section>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.header {
+    position: sticky;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 30px;
+    width: 100vw;
+    height: 10vh;
+    color: var(--secondary-color);
+    background-color: var(--primary-color);
+    border-bottom: 1px solid var(--deeper-primary);
+
+    @media screen and (min-width: 768px) {
+        padding-right: 50px;
+    }
+
+    .logo {
+        text-transform: none;
+        cursor: pointer;
+
+        .shadow {
+            float: right;
+            margin-left: -5px;
+            font-style: italic;
+            color: var(--transparent-secondary);
+        }
+
+        .noumenon {
+            color: var(--secondary-color);
+        }
+    }
+
+    .navis {
+        .tabs {
+            display: flex;
+
+            @media screen and (max-width: 767px) {
+                display: none;
+            }
+
+            .navi-tab {
+                background-color: var(--primary-color);
+            }
+        }
+
+        .drawer {
+            padding: 10px;
+            width: 100%;
+            color: var(--secondary-color);
+            background-color: var(--primary-color);
+        }
+
+        .tab-folder {
+            font-size: large;
+            color: var(--secondary-color);
+
+            @media screen and (min-width: 768px) {
+                display: none;
+            }
+        }
+    }
+}
+</style>
