a767ef4f9f4a459f6ab87cd3161bcbb1f3e1bd08
abuliegeguolage <123>
2024-12-22 21:44:01 +0800

    feat: install

---
+++ src/pages/Install.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const tab = ref(null);
+</script>
+
+<template>
+    <h3 class="big-title">
+        如何將Mavis加入您的團隊？
+    </h3>
+
+    <v-card class="card">
+        <v-tabs v-model="tab" class="tab-box">
+            <v-tab value="jobBank" class="tab">104 人力銀行</v-tab>
+            <v-tab value="email" class="tab">電子信箱</v-tab>
+        </v-tabs>
+        <v-card-text>
+            <v-tabs-window v-model="tab">
+                <v-tabs-window-item value="jobBank" class="text">
+                    歡迎透過
+                    <a href="https://www.104.com.tw">104 人力銀行</a>
+                    發出邀請！
+                </v-tabs-window-item>
+
+                <v-tabs-window-item value="email" class="text">
+                    寄信到 mavis48937078@gmail.com 即可～
+                </v-tabs-window-item>
+            </v-tabs-window>
+        </v-card-text>
+    </v-card>
+</template>
+
+<style scoped lang="scss">
+h3 {
+    padding-top: 20vh;
+}
+
+.card {
+
+    .tab-box {
+        background-color: var(--deeper-primary);
+
+        .tab {
+            color: var(--secondary-color);
+        }
+    }
+
+    .text {
+        cursor: text;
+        color: var(--primary-color);
+
+        a {
+            text-decoration: underline;
+            color: var(--deeper-primary);
+            border-radius: 6px;
+            transition: .3s;
+
+            &:hover {
+                background-color: var(--secondary-color);
+                box-shadow: 0 0 10px var(--secondary-color);
+            }
+        }
+    }
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 
 import Home from './pages/Home.vue'
+import Install from './pages/Install.vue'
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/install', component: Install },
 ]
 
 export const router = createRouter({
